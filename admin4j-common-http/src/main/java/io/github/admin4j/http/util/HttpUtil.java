cb7c5d77424701a65aa416baba3d61ebf52ba75f
andyoung <1218853253@qq.com>
2024-06-13 14:30:00 +0800

    get 文档

--- admin4j-common-http/src/main/java/io/github/admin4j/http/util/HttpUtil.java
+++ admin4j-common-http/src/main/java/io/github/admin4j/http/util/HttpUtil.java
@@ -43,58 +43,139 @@ public class HttpUtil {
         SINGLETON_REQUEST = apiJsonClient;
     }
 
+    /**
+     * get 请求
+     * @param url
+     * @param queryParams 查询参数
+     * @return
+     */
     public static Response get(String url, Pair<?>... queryParams) {
 
         return getClient().get(url, (Map<String, Object>) null, queryParams);
     }
 
 
+    /**
+     * get 请求
+     * @param url
+     * @param queryParams 查询参数
+     * @return
+     */
     public static Response get(String url, Map<String, Object> queryParams) {
 
         return getClient().get(url, queryParams, (Pair<?>[]) null);
     }
 
 
+    /**
+     * post 请求
+     * @param url
+     * @param body post body 体
+     * @return
+     */
     public static Response post(String url, Object body) {
         return getClient().post(url, MediaTypeEnum.JSON, body, (Map<String, Object>) null, (Map<String, Object>) null);
     }
 
+    /**
+     * post（application/json;） 请求
+     * @param url
+     * @param body  post body 体
+     * @param header header 头
+     * @return
+     */
     public static Response post(String url, Object body, Map<String, Object> header) {
         return getClient().post(url, MediaTypeEnum.JSON, body, (Map<String, Object>) null, header);
     }
 
+    /**
+     *  form（x-www-form-urlencoded） 格式的 post 请求
+     * @param url
+     * @param formParams
+     * @return
+     */
     public static Response postForm(String url, Map<String, Object> formParams) {
         return getClient().post(url, MediaTypeEnum.FORM, null, formParams, (Map<String, Object>) null);
     }
 
+    /**
+     * form（x-www-form-urlencoded） 格式的 post 请求
+     * @param url
+     * @param formParams
+     * @param header
+     * @return
+     */
     public static Response postForm(String url, Map<String, Object> formParams, Map<String, Object> header) {
         return getClient().post(url, MediaTypeEnum.FORM, null, formParams, header);
     }
 
+    /**
+     * put 请求
+     * @param url
+     * @param body
+     * @return
+     */
     public static Response put(String url, Object body) {
         return getClient().put(url, MediaTypeEnum.JSON, body, (Map<String, Object>) null, (Map<String, Object>) null);
     }
 
+    /**
+     * put 请求
+     * @param url
+     * @param body
+     * @param header
+     * @return
+     */
     public static Response put(String url, Object body, Map<String, Object> header) {
         return getClient().put(url, MediaTypeEnum.JSON, body, (Map<String, Object>) null, header);
     }
 
+    /**
+     * form（x-www-form-urlencoded） 格式的 put 请求
+     * @param url
+     * @param formParams
+     * @return
+     */
     public static Response putForm(String url, Map<String, Object> formParams) {
         return getClient().put(url, MediaTypeEnum.FORM, null, formParams, (Map<String, Object>) null);
     }
 
+    /**
+     * form（x-www-form-urlencoded） 格式的 put 请求
+     * @param url
+     * @param formParams
+     * @return
+     */
     public static Response putForm(String url, Map<String, Object> formParams, Map<String, Object> header) {
         return getClient().put(url, MediaTypeEnum.FORM, null, formParams, header);
     }
 
+    /**
+     * delete 请求
+     * @param url
+     * @param body
+     * @return
+     */
     public static Response delete(String url, Object body) {
         return getClient().delete(url, MediaTypeEnum.JSON, body, (Map<String, Object>) null, (Map<String, Object>) null);
     }
 
+    /**
+     * delete 请求
+     * @param url
+     * @param body
+     * @return
+     */
     public static Response delete(String url, Object body, Map<String, Object> header) {
         return getClient().delete(url, MediaTypeEnum.JSON, body, (Map<String, Object>) null, header);
     }
 
+    /**
+     * form（x-www-form-urlencoded） 格式的 delete 请求
+     * @param url
+     * @param formParams
+     * @return
+     */
     public static Response deleteForm(String url, Map<String, Object> formParams) {
         return getClient().delete(url, MediaTypeEnum.FORM, null, formParams, (Map<String, Object>) null);
     }
